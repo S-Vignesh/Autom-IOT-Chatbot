b5336f163abd7d6cafe467458773ecd3c230bccd
Abhishek Raj <raj.abhishek.01.1@gmail.com>
2017-03-18 16:24:59 +0530

    fir

--- conf.js
+++ conf.js
@@ -2,5 +2,5 @@ var conf = module.exports = {
 	FB_MESSAGE_URL: 'https://graph.facebook.com/v2.6/me/messages',
 	VERIFY_TOKEN: 'abhishek_vignesh',
 	PROFILE_TOKEN: 'EAALHX1UyRuoBACPdVcJjZC3Rv6vnxDDsJmsUoZA8DaWrMmyWkWn9wNql2eT5LpQDjaCwQZBJXhWCVLOs8rvWhO3LQzdZAYNJitinj2Qxw2MiOuMBZBeO5eg9szDnDZCDENoEdrw1W7WJatdz0lVZBi8iGhGtrSPLg3aaVexsy7ELwZDZD',
-	PORT: '3000'
+	PORT: process.env.PORT || '3000'
 };
